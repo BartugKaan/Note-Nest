09eb71e4180ca455db4bb7bd3203316baf9a5fc3
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-02-23 01:18:08 +0300

    Add POST route for creating new notes with validation

--- backend/src/routes/noteRoutes.js
+++ backend/src/routes/noteRoutes.js
@@ -13,4 +13,23 @@ router.get('/', async (req, res) => {
   }
 })
 
+//Post(/api/notes)
+router.post('/', async (req, res) => {
+  try {
+    const { title, content } = req.body
+
+    if (!title || !content) {
+      return res.status(400).json({ error: 'Title and Content required!' })
+    }
+
+    const newNote = await prisma.note.create({
+      data: { title, content },
+    })
+    res.status(201).json(newNote)
+  } catch (error) {
+    console.error('Error occured while created new Note!', error)
+    res.status(500).json({ error: 'Error happend' })
+  }
+})
+
 module.exports = router
