4654ae9a564644b314de8fc6487fef2bbf906351
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-02-25 14:47:54 +0300

    Add routing and lottie-react dependency to frontend

--- notes-app-frontend/src/App.tsx
+++ notes-app-frontend/src/App.tsx
@@ -1,12 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600 fw-900">
-        Tailwind is Working! 🥳
-      </h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
   )
 }
 
